286c7347d355dea0f77906cc27818066e3e4c221
MsNahid <mssnahid729@gmail.com>
2020-10-27 21:48:02 +0600

    functionality hovering over columns

--- Version 1/connect4.js
+++ Version 1/connect4.js
@@ -4,6 +4,7 @@ class Connect4{
         this.COLS = 7;
         this.selector = selector;
         this.createGrid();
+        this.setUpEventListeners();
     }
     
     createGrid(){
@@ -11,11 +12,40 @@ class Connect4{
         for(let row = 0; row < this.ROWS; row++){
             const $row = $('<div>').addClass('row');
             for(let col = 0; col < this.COLS; col++){
-                const $col = $('<div>').addClass('col empty');
+                const $col = $('<div>')
+                .addClass('col empty')
+                .attr('data-col', col)
+                .attr('data-row', row);
                 $row.append($col);
             }
             $board.append($row);
         }
-        console.log($board.html());
+        // console.log($board.html());
+    }
+
+    setUpEventListeners(){
+        const $board = $(this.selector);
+        
+        function findLastEmptyCell(col) {
+            const cells = $(`.col[data-col='${col}']`);
+            for(let i = cells.length - 1; i >= 0; i--){
+                const $cell = $(cells[i]);
+                if($cell.hasClass('empty')){
+                    return $cell;
+                }
+            }
+            return null;
+        }
+        $board.on('mouseenter', '.col.empty', function(){
+            // console.log('here', this);
+            const col = $(this).data('col');
+            // console.log(col);
+            const $lastEmptyCell = findLastEmptyCell(col);
+            $lastEmptyCell.addClass('next_red');
+        })  
+
+        $board.on("mouseleave", ".col", function(){
+            $('.col').removeClass('next_red');
+        }) 
     }
 }
