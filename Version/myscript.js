61d3ecc4daef0f3c125597657b3715ac773367f3
MsNahid <mssnahid729@gmail.com>
2020-10-31 08:44:05 +0600

    Created grid

---
+++ Version/myscript.js
@@ -0,0 +1,6 @@
+var player1 = prompt("Enter you name, your color will be blue!")
+var player1Color = 'rgb(86, 151, 255)';
+
+var player2 = prompt("Enter you name, your color will be red!")
+var player1Color = 'rgb(237, 45, 37)';
+
